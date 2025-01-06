2c2f0e5e0876a1c29fc6e2792af5bbe649b0e04a
Katherine Shaw <135996296+irr3placeable@users.noreply.github.com>
2025-01-05 19:45:37 -0500

    Add files via upload

---
+++ Arithmetic Worksheet/Arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        System.out.println(2 * 3);
+        System.out.println(2 + 3 * 5);
+        System.out.println((2 + 13) / 5);
+        System.out.println(25);
+        System.out.println(7 / 3 - 1);
+        System.out.println((7 - 12) / (6 - 1));
+    }
+}
